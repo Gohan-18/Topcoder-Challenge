799775a212cdc6a77d0abe2f21ec28c041be2446
Prabhat <112978778+Gohan-18@users.noreply.github.com>
2023-01-23 12:44:46 +0530

    done

--- Topcoder-challenge/src/Components/Profile.jsx
+++ Topcoder-challenge/src/Components/Profile.jsx
@@ -1,16 +1,18 @@
-import { InputLabel, MenuItem, Select, Typography } from '@mui/material';
+import { Autocomplete, InputLabel, MenuItem, Select, Typography } from '@mui/material';
 import { Box, Container, TextField, Button, IconButton } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { db, useAuth } from '../firebase/Auth';
 import ChevronLeftRoundedIcon from '@mui/icons-material/ChevronLeftRounded';
 import { Form, useNavigate } from 'react-router-dom';
 import Avatar from '@mui/material/Avatar';
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs } from 'firebase/firestore';
+import CircularProgress from '@mui/material/CircularProgress';
 
 export default function Profile() {
 
   // const [country, setCountry] = useState('');
   const [userDetails, setUserDetails] = useState([]);
+  const [tags, setTags] = useState(() => {tags: []});
   const [edit, setEdit] = useState(true);
 
   async function getUserDetails () {
@@ -68,7 +70,13 @@ export default function Profile() {
       <IconButton onClick={navigateToMyAccount} sx={{position: 'static'}} >
         <ChevronLeftRoundedIcon fontSize='large'/>
       </IconButton>
-    <Container maxWidth='sm'  >
+    {!userDetails.length ? 
+      <Box  pt={6} sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+        }}><CircularProgress/></Box>  : 
+      <Container maxWidth='sm'  >
         <Box  pb={6} sx={{
             display: 'flex',
             alignItems: 'center',
@@ -133,20 +141,23 @@ export default function Profile() {
             </Box>
             <Box sx={{display: 'flex', alignItems: 'flex-start', justifyContent: 'flex-start', flexDirection: 'column', mb: '20px'}} >
             <Typography gutterBottom sx={{fontSize: '15px', pl: '5px'}} >Interests</Typography>
-              <TextField
-                defaultValue={userData?.data?.interests}
-                multiline
-                minRows={4}
-                autoComplete='off'
-                name='interests'
-                id="interests"
-                // label='Interests'
-                InputProps={{
-                  readOnly: edit,
-                }}
+              <Autocomplete
+                disabled
                 sx={{
-                  width:'100%'
+                  width: '100%'
                 }}
+                multiple
+                id="interests"
+                options={userData?.data?.interests}
+                // getOptionLabel={(option) => option}
+                defaultValue={[...userData?.data?.interests]}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    disabled
+                    sx={{width: '100%',color: 'black'}}
+                  />
+                )}
               />
             </Box>
             {/* {!edit && <Button type='submit' fullWidth variant='contained' color='success'>Update</Button>} */}
@@ -156,7 +167,7 @@ export default function Profile() {
         {/* <Container maxWidth='lg'>
           <Button fullWidth variant='contained' color='error' onClick={logOut}>LogOut</Button>
         </Container> */}
-    </Container>
+    </Container>}
     </Container>
     </>
   )
